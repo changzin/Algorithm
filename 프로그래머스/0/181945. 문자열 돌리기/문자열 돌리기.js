b4292e934b6b8c6c4ac75a0a211969ec4cd615b5
changzin <changzin0126@gmail.com>
2024-04-02 09:07:21 +0900

    [level 0] Title: 문자열 돌리기, Time: 52.60 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    for(let j = 0; j < str.length; j++){
+        console.log(str[j])
+    }
+});
