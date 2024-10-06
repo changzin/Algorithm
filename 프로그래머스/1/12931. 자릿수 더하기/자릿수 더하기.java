4e08aff54dab08d1173cf3034cab54929a1fe086
changzin <changzin0126@gmail.com>
2024-10-06 22:41:06 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int rest = 0;
+        while(n != 0){
+            rest = n % 10;
+            answer += rest;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
